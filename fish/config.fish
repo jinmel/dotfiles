86de2210d0b3b9f404dc9544300bf8031a75860f
Jin Suk Park <totorovirus@gmail.com>
2016-02-29 16:43:19 +0900

    omf -> fisher

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,6 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH "/home/crank/.local/share/omf"
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/home/crank/.config/omf"
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-
 set -x EDITOR vim
 set PATH ~/.local/bin/ $PATH
 set -x BROWSER firefox
+set fisher_home ~/.local/share/fisherman
+set fisher_config ~/.config/fisherman
+source $fisher_home/config.fish
