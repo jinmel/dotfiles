5a413f9cb15fe840236f4827cf2fe5c9af0f92fb
Jin Suk Park <totorovirus@gmail.com>
2016-06-01 20:26:16 -0700

    remove fish

--- fish/config.fish
+++
@@ -1,6 +0,0 @@
-set -x EDITOR vim
-set PATH ~/.local/bin/ $PATH
-set -x BROWSER firefox
-set fisher_home ~/.local/share/fisherman
-set fisher_config ~/.config/fisherman
-source $fisher_home/config.fish
